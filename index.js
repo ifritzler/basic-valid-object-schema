86ef10d36232fe754ee7311591b77512d752f041
ifritzler <contacto.fritzlerilan@gmail.com>
2023-03-18 01:20:36 -0300

    refactor: :recycle: refactor all code base

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ class ValidationObject {
     this.#options = {}
   }
 
+  /**
+   * Crea el esquema de validación a partir del esquema de entrada.
+   * @private
+   * @param {object} schema - El esquema de entrada.
+   * @returns {object} - El esquema de validación creado.
+   * @throws {Error} - Si el esquema de entrada no es válido.
+   */
   #createSchema (schema) {
     const newSchema = {}
-
     for (const [key, value] of Object.entries(schema)) {
       if (typeof value === 'object') {
         if (value.type === 'array') {
@@ -23,7 +29,6 @@ class ValidationObject {
             throw new Error('A schema of an array must be a valid schema object or string primitive.')
           }
           const childSchema = typeof value.schema === 'object' ? this.#createSchema(value.schema) : value.schema
-
           newSchema[key] = {
             type: 'array',
             isArray: true,
@@ -37,7 +42,6 @@ class ValidationObject {
           }
         } else {
           const childSchema = this.#createSchema(value.schema)
-
           newSchema[key] = {
             type: 'object',
             required: value.required ?? true,
@@ -52,68 +56,48 @@ class ValidationObject {
       } else {
         throw new Error('If prop are from type array, the schema needs to be an object with the type array and a valid schema')
       }
-
       if (value && value.default) {
         newSchema[key].default = value.default
       }
     }
-
     return newSchema
   }
 
+  /**
+   * Crea un objeto a partir de un conjunto de claves.
+   * @private
+   * @param {string[]} keys - Las claves para crear el objeto.
+   * @param {object} [lastObj={}] - El último objeto en la cadena de objetos creada.
+   * @returns {object} - El objeto creado.
+   */
   #createObjectFromKeys (keys, lastObj = {}) {
     return keys.reduceRight((acc, currentKey) => ({ [currentKey]: acc }), lastObj)
   }
 
+  /**
+   * Valida un objeto contra un esquema de validación.
+   * @private
+   * @param {object} obj - El objeto a validar.
+   * @param {object|string} schema - El esquema de validación.
+   * @param {string[]} [parentKeys=null] - Las claves del objeto padre.
+   * @returns {boolean} - `true` si el objeto es válido, `false` en caso contrario.
+   */
   #validateObjectAgainstSchema (obj, schema, parentKeys = null) {
     if (typeof schema === 'string' && typeof obj !== 'object') {
-      if (typeof obj !== schema) {
-        this.#errors = this.#createObjectFromKeys([...parentKeys ?? []], {
-          error: `item with value '${obj}' of array must be a valid ${schema}.`
-        })
-        return false
-      }
+      return this.#validateObjectAgainstSchemaString(obj, schema, parentKeys)
     } else {
       for (const key in schema) {
-        // Si la propiedad no está en el objeto, devolver false si es requerida, de lo contrario, pasar a la siguiente propiedad
-        if (typeof obj === 'object' && !Array.isArray(obj) && !(key in obj)) {
+        if (!(key in obj)) {
           if (schema[key].required) {
-            if (parentKeys === null) {
-              this.#errors[key] = {
-                error: 'Is required'
-              }
-              return false
-            }
-            this.#errors = this.#createObjectFromKeys([...parentKeys ?? [], key], {
-              error: 'Is required'
-            })
+            this.#setErrorForKey(parentKeys, key, 'Is required')
             return false
           } else {
             continue
           }
         }
-        // Verificar el tipo de dato
-        // eslint-disable-next-line valid-typeof
-        if (typeof obj[key] !== schema[key].type) {
-          if (!Array.isArray(obj[key])) {
-            if (parentKeys === null) {
-              this.#errors[key] = {
-                error: `${key} must be a valid ${schema[key].type}.`
-              }
-            } else {
-              this.#errors = this.#createObjectFromKeys([...parentKeys, key], {
-                error: `${key} must be a valid ${schema[key].type}.`
-              })
-            }
-            return false
-          }
-          for (let i = 0; i < obj[key].length; i++) {
-            if (!this.#validateObjectAgainstSchema(obj[key][i], schema[key].itemSchema, [...parentKeys ?? [], key])) {
-              return false
-            }
-          }
+        if (!this.#validateObjectKeyType(obj[key], schema[key], parentKeys, key)) {
+          return false
         }
-        // Verificar el esquema anidado
         if (schema[key].schema && typeof obj[key] === 'object') {
           if (!this.#validateObjectAgainstSchema(obj[key], schema[key].schema, [...parentKeys ?? [], key])) {
             return false
@@ -121,10 +105,75 @@ class ValidationObject {
         }
       }
     }
-    // Si llegamos aquí, el objeto coincide con el esquema
     return true
   }
 
+  /**
+   * Valida un objeto contra un esquema de validación representado como una cadena de caracteres.
+   * @private
+   * @param {any} obj - El objeto a validar.
+   * @param {string} schema - El esquema de validación representado como una cadena de caracteres.
+   * @param {string[]} parentKeys - Las claves del objeto padre.
+   * @returns {boolean} - `true` si el objeto es válido, `false` en caso contrario.
+   */
+  #validateObjectAgainstSchemaString (obj, schema, parentKeys) {
+    if (typeof obj !== schema) {
+      this.#setErrorForKey(parentKeys, null, `item with value '${obj}' of array must be a valid ${schema}.`)
+      return false
+    }
+    return true
+  }
+
+  /**
+   * Valida el tipo de una propiedad del objeto contra su tipo esperado según el esquema de validación.
+   * @private
+   * @param {any} value - El valor de la propiedad.
+   * @param {object} schema - El esquema de validación para la propiedad.
+   * @param {string[]} parentKeys - Las claves del objeto padre.
+   * @param {string} key - La clave de la propiedad.
+   * @returns {boolean} - `true` si el valor es del tipo esperado, `false` en caso contrario.
+   */
+  #validateObjectKeyType (value, schema, parentKeys, key) {
+    if (typeof value !== schema.type) {
+      if (!Array.isArray(value)) {
+        this.#setErrorForKey([...parentKeys ?? [], key], null, `${key} must be a valid ${schema.type}.`)
+        return false
+      }
+      for (let i = 0; i < value.length; i++) {
+        if (!this.#validateObjectAgainstSchema(value[i], schema.itemSchema, [...parentKeys ?? [], key])) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  /**
+   * Setea un error en el objeto de errores `#errors` en la clave dada por `parentKeys` y `key`.
+   * Si `parentKeys` es `null`, entonces la clave del error será `key`.
+   * Si `key` es `null`, entonces se trata de un error genérico para todos los elementos de un array.
+   *
+   * @private
+   * @param {?string[]} parentKeys - Un array con las claves de las propiedades padres del objeto, en orden descendente.
+   * @param {?string} key - La clave de la propiedad del objeto donde ocurrió el error.
+   * @param {string} error - El mensaje de error a asignar.
+   * @returns {void}
+   */
+  #setErrorForKey (parentKeys, key, error) {
+    if (parentKeys === null) {
+      this.#errors = { [key]: { error } }
+    } else {
+      const resolvedKeys = key ? [...parentKeys, key] : [...parentKeys]
+      this.#errors = this.#createObjectFromKeys([...resolvedKeys], { error })
+    }
+  }
+
+  /**
+   * Asigna los valores por defecto para las propiedades del objeto.
+   * @private
+   * @param {object} schema - El esquema de validación.
+   * @param {object} obj - El objeto a asignar valores por defecto.
+   */
   #asignedDefaultValues (schema, obj) {
     for (const [prop, { default: defaultValue }] of Object.entries(schema)) {
       if (!(prop in obj)) {
@@ -137,24 +186,28 @@ class ValidationObject {
     }
   }
 
-  // Si required no viene definido es true
-  // Las props del schema pueden ser un string indicando el type o un objeto con props {required, type, }
-
+  /**
+   * Setea un error en el objeto de errores `#errors` en la clave dada por `parentKeys` y `key`.
+   * Si `parentKeys` es `null`, entonces la clave del error será `key`.
+   * Si `key` es `null`, entonces se trata de un error genérico para todos los elementos de un array.
+   *
+   * @private
+   * @param {?string[]} parentKeys - Un array con las claves de las propiedades padres del objeto, en orden descendente.
+   * @param {?string} key - La clave de la propiedad del objeto donde ocurrió el error.
+   * @param {string} error - El mensaje de error a asignar.
+   * @returns {void}
+   */
   validate (obj, depth = 0, schema = this.#schema) {
     this.#asignedDefaultValues(schema, obj)
-    // Elimina propiedades que no esten dentro del schema
     for (const prop in obj) {
       if (typeof obj[prop] === 'object' && obj[prop] !== null) {
-        // Si la propiedad es un objeto, llamar recursivamente la función `validate`
         if (!Array.isArray(obj[prop])) {
           this.validate(obj[prop], depth + 1, schema[prop]?.schema)
         }
       } else if (!schema[prop]) {
-        // Si la propiedad no está definida en el schema, eliminarla
         delete obj[prop]
       }
     }
-    // Valida contra el schema si las propiedades cumplen con lo solicitado
     this.#validateObjectAgainstSchema(obj, schema)
     return {
       isValidate: Object.entries(this.#errors).length === 0,
@@ -165,10 +218,6 @@ class ValidationObject {
 
   getSchema () {
     return this.#schema
-  }
-
-  getErrors () {
-    return this.#errors
   }
 
   blankErrors () {
